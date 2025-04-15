5fad5a188d9e4890ba42b3a419f5e840b9b13732
soooogo <sougo.sougo.0312@gmail.com>
2025-04-15 16:15:57 +0900

    2025/4/15

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,14 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: 'Invoices | edit',
+};
+
+
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
     const id = params.id;
@@ -9,6 +16,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         fetchInvoiceById(id),
         fetchCustomers(),
       ]);
+    if (!invoice) {
+      notFound();
+    }
+
   return (
     <main>
       <Breadcrumbs
